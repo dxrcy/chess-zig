50766770cc523faff46a1d6d1e54b897e0831d73
darcy <acednes@gmail.com>
2025-11-04 17:43:31 +1100

    feat(moves): add pawn en passant

--- src/Board.zig
+++ src/Board.zig
@@ -144,17 +144,23 @@ pub const AvailableMoves = struct {
             const move = moves[self.index];
             self.index += 1;
 
-            const tile_future = move.offset.applyTo(self.tile, piece) orelse {
+            const destination = move.offset.applyTo(self.tile, piece) orelse {
                 continue;
             };
 
-            if (move.requirement.isSatisfied(.{
+            const context = Requirement.Context{
                 .board = self.board,
                 .piece = piece,
-                .tile_current = self.tile,
-                .tile_future = tile_future,
-            })) {
-                return tile_future;
+                .tile = self.tile,
+                .move = move,
+                .destination = destination,
+            };
+
+            if (move.requirement.isSatisfied(context)) {
+                // TODO: Return some result type which includes `tile_future`
+                // and whether any piece was taken (if different).
+                // Possibly derived from `move`
+                return destination;
             }
         }
 
@@ -182,6 +188,16 @@ fn getMoves(piece: Piece.Kind) []const Move {
                 .offset = .{ .advance = .{ .rank = 1, .file = 1 } },
                 .requirement = .{ .take = .always },
             },
+            .{
+                .offset = .{ .advance = .{ .rank = 1, .file = -1 } },
+                .requirement = .{ .take = .always },
+                .take = .{ .real = .{ .rank = 0, .file = -1 } },
+            },
+            .{
+                .offset = .{ .advance = .{ .rank = 1, .file = 1 } },
+                .requirement = .{ .take = .always },
+                .take = .{ .real = .{ .rank = 0, .file = 1 } },
+            },
         },
         else => &[_]Move{},
     };
@@ -190,16 +206,21 @@ fn getMoves(piece: Piece.Kind) []const Move {
 const Move = struct {
     offset: Offset,
     requirement: Requirement,
+    take: ?Offset = null,
 };
 
 /// An offset which is dependant on the piece's attributes (eg. color).
 // TODO: (DO LATER) Depending on how many variants are added, this abstraction
 // could possibly be removed or improved somehow.
 const Offset = union(enum) {
+    real: RealOffset,
     advance: RealOffset,
 
     pub fn applyTo(self: Offset, tile: Tile, piece: Piece) ?Tile {
         switch (self) {
+            .real => |offset| {
+                return offset.applyTo(tile);
+            },
             .advance => |offset| {
                 var real_offset = offset;
                 real_offset.rank *= if (piece.player == .white) 1 else -1;
@@ -242,42 +263,35 @@ const Requirement = struct {
     /// For a pawn's first move.
     home_rank: ?usize = null,
 
-    const Context = struct {
-        board: *const Board,
-        piece: Piece,
-        // TODO: Rename?
-        tile_current: Tile,
-        tile_future: Tile,
-    };
-
-    // TODO: Add custom field for special behaviour (eg. en passant, castling).
+    // TODO: Add custom field for special behaviour (eg. castling). IF NECESSARY
 
     pub fn isSatisfied(
         self: *const Requirement,
         context: Context,
     ) bool {
         // Can never take/overwrite own piece
-        if (context.board.get(context.tile_future)) |piece_take| {
+        if (context.board.get(context.destination)) |piece_take| {
             if (piece_take.player == context.piece.player) {
                 return false;
             }
         }
 
         if (self.take) |take| {
-            const should_take = switch (take) {
-                .always => true,
-                .never => false,
+            const will_take = context.willTake();
+            const satisfied = switch (take) {
+                .always => will_take == .take,
+                .never => will_take == .no_take,
             };
-            if (should_take != willTake(context)) {
+            if (!satisfied) {
                 return false;
             }
         }
 
         if (self.home_rank) |home_rank| {
             const actual_home_rank = if (context.piece.player == .white)
-                context.tile_current.rank
+                context.tile.rank
             else
-                Board.SIZE - context.tile_current.rank - 1;
+                Board.SIZE - context.tile.rank - 1;
             if (home_rank != actual_home_rank) {
                 return false;
             }
@@ -286,10 +300,31 @@ const Requirement = struct {
         return true;
     }
 
-    fn willTake(context: Context) bool {
-        const piece_take = context.board.get(context.tile_future) orelse
-            return false;
-        assert(piece_take.player != context.piece.player);
-        return true;
-    }
+    const Context = struct {
+        board: *const Board,
+        piece: Piece,
+        tile: Tile,
+        move: Move,
+        destination: Tile,
+
+        fn willTake(self: *const Context) enum { invalid, take, no_take } {
+            const tile_take = self.getTakeTile() orelse {
+                return .invalid;
+            };
+            const piece_take = self.board.get(tile_take) orelse {
+                return .no_take;
+            };
+            if (piece_take.player == self.piece.player) {
+                return .invalid;
+            }
+            return .take;
+        }
+
+        fn getTakeTile(self: *const Context) ?Tile {
+            if (self.move.take) |take| {
+                return take.applyTo(self.tile, self.piece);
+            }
+            return self.destination;
+        }
+    };
 };
